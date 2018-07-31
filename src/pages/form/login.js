b9de117dc71f4981f6bf29c466b94d23af40bf46
qixin <qixin@haoqix.com>
2018-07-31 22:55:41 +0800

    t0.6.2

--- src/pages/form/login.js
+++ src/pages/form/login.js
@@ -2,12 +2,22 @@
  * create at 07/30/18
  */
 import React, { Component } from 'react'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message, Icon, Checkbox } from 'antd'
 
 // const 
 const FormItem = Form.Item
 
 class FormLogin extends Component{
+	_handleSubmit = ()=>{
+		let userInfo = this.props.form.getFieldsValue()
+		this.props.form.validateFields((err, values)=>{
+			if(!err){
+				message.success(`account=${userInfo.userName} password=${userInfo.password}`)
+			}
+
+		})
+	}
+
 	render(){
 		const { getFieldDecorator } = this.props.form
 		return (
@@ -30,25 +40,50 @@ class FormLogin extends Component{
 						<FormItem >
 							{
 								getFieldDecorator('userName', {
-									initialValue: 'Jack',
-									rules: []
+									initialValue: '',
+									rules: [
+										{
+											required: true,
+											message: '用户名不能为空'
+										},
+										{
+											min: 5, max: 10,
+											message: '长度不在范围内'
+										},
+										{
+											pattern: /^\w+$/g,
+											message: '用户名必须为字母或者数字'
+										}
+									]
 								})(
-									<Input placeholder="请输入账号"/>
+									<Input prefix={<Icon type="user"/>} placeholder="请输入账号"/>
 								)
 							}
 						</FormItem>
 						<FormItem >
 							{
 								getFieldDecorator('password', {
-									initialValue: '123456',
+									initialValue: '',
 									rules: []
 								})(
-									<Input placeholder="请输入密码"/>
+									<Input prefix={<Icon type="lock"/>} type="password" placeholder="请输入密码"/>
 								)
 							}
 						</FormItem>
 						<FormItem >
-							<Button type="primary">登录</Button>
+							{
+								getFieldDecorator('remember', {
+									valuePropName: 'checked',
+									initialValue: true,
+									rules: []
+								})(
+									<Checkbox>记住密码.</Checkbox>
+								)
+							}
+							<a href="#" style={{float: 'right'}}>忘记密码</a>
+						</FormItem>
+						<FormItem >
+							<Button type="primary" onClick={this._handleSubmit}>登录</Button>
 						</FormItem>
 					</Form>
 				</Card>
