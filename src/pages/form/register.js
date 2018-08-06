8892348c7f0106f75b6492d7405fd0943efa17aa
qixin <qixin@haoqix.com>
2018-08-06 23:15:36 +0800

    t0.6.4

--- src/pages/form/register.js
+++ src/pages/form/register.js
@@ -3,15 +3,44 @@
  */
 import React, { Component } from 'react'
 import { 
-	Card, Form, Input, Select, Radio, InputNumber,
+	Card, Form, Input, Select, Radio, InputNumber, Switch, DatePicker, TimePicker,
+	Upload, Icon, Checkbox, Button,
 } from 'antd'
+import moment from 'moment'
 
 // const 
 const FormItem = Form.Item
 const RadioGroup = Radio.Group
 const SelectOption = Select.Option
+const TextArea = Input.TextArea
 
 class FormRegister extends Component{
+	state ={}
+
+	getBase64(img, callback) {
+		const reader = new FileReader()
+		reader.addEventListener('load', () => callback(reader.result))
+		reader.readAsDataURL(img)
+	}
+
+	_handleChange = (info)=>{
+		if (info.file.status === 'uploading') {
+      this.setState({ loading: true })
+      return
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      this.getBase64(info.file.originFileObj, imageUrl => this.setState({
+        userImg: imageUrl,
+        loading: false,
+      }))
+    }
+	}
+
+	_registerClick = ()=>{
+		let userInfo = this.props.form.getFieldsValue()
+		console.log('_registerClick userInfo=>', userInfo)
+	}
 
 	render(){
 		const { getFieldDecorator } = this.props.form
@@ -23,6 +52,15 @@ class FormRegister extends Component{
 			wrapperCol: {
 				xs: 24,
 				sm: 8
+			}
+		}
+		const offsetLayout = {
+			wrapperCol: {
+				xs: 24,
+				sm: {
+					span: 8,
+					offset: 4	
+				}
 			}
 		}
 		return (
@@ -106,6 +144,85 @@ class FormRegister extends Component{
 									</Select>
 								)
 							}
+						</FormItem>
+						<FormItem label="是否已婚" {...formItemLayout}>
+							{
+								getFieldDecorator('isMarried', {
+									valuePropName: 'checked',
+									initialValue: true
+								})(
+									<Switch />
+								)
+							}
+						</FormItem>
+						<FormItem label="生日" {...formItemLayout}>
+							{
+								getFieldDecorator('birthday', {
+									initialValue: moment('2018-08-08')
+								})(
+									<DatePicker 
+										showTime
+										format='YYYY-MM-DD HH:mm:ss'
+									/>
+								)
+							}
+						</FormItem>
+						<FormItem label="地址" {...formItemLayout}>
+							{
+								getFieldDecorator('address', {
+									initialValue:''
+								})(
+									<TextArea 
+										autosize={{
+											minRows: 2, maxRows: 6
+										}}
+									/>
+								)
+							}
+						</FormItem>
+						<FormItem label="早起时间" {...formItemLayout}>
+							{
+								getFieldDecorator('getUpTime')(
+									<TimePicker 
+										format='HH:mm'
+									/>
+								)
+							}
+						</FormItem>
+						<FormItem label="头像" {...formItemLayout}>
+							{
+								getFieldDecorator('userImg', {
+									initialValue: ''
+								})(
+									<Upload 
+										listType="picture-card"
+										showUploadList={false}
+										action="//jsonplaceholder.typicode.com/posts/"
+										onChange={this._handleChange}
+									>
+										{
+											this.state.userImg ? 
+											<img src={this.state.userImg}/> : 
+											<Icon type='plus'/>
+										}
+									</Upload>
+								)
+							}
+						</FormItem>
+						<FormItem {...offsetLayout}>
+							{
+								getFieldDecorator('contract', {
+									valuePropName: 'checked',
+									initialValue: false
+								})(
+									<Checkbox >
+										我已经阅读过<a href="#">注册协议</a>
+									</Checkbox>
+								)
+							}
+						</FormItem>
+						<FormItem {...offsetLayout}>
+							<Button type='primary' onClick={this._registerClick}>注册</Button>
 						</FormItem>
 					</Form>
 				</Card>
